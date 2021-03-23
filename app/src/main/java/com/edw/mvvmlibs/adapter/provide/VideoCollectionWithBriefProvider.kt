eb7c0d7b0cf732215738302cb04dae0642d11bdf
Edwardwmd <1732141816wmd@gmail.com>
2021-03-23 23:28:14 +0800

    Realize pull-down refresh, item layout, data display

--- app/src/main/java/com/edw/mvvmlibs/adapter/provide/VideoCollectionWithBriefProvider.kt
+++ app/src/main/java/com/edw/mvvmlibs/adapter/provide/VideoCollectionWithBriefProvider.kt
@@ -1,12 +1,16 @@
 package com.edw.mvvmlibs.adapter.provide
 
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.chad.library.adapter.base.provider.BaseItemProvider
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.edw.mvvmlibs.R
 import com.edw.mvvmlibs.adapter.CardViewType
-import com.edw.mvvmlibs.entity.HomeBaseItem
+import com.edw.mvvmlibs.adapter.VideoCollectionWithBriefFooterAdapter
 import com.edw.mvvmlibs.databinding.ItemVideocollectionwithbriefBinding
+import com.edw.mvvmlibs.entity.HomeBaseItem
+
+import com.edw.mvvmlibs.entity.CollectionItemCard
 
 class
 
@@ -23,6 +27,30 @@ VideoCollectionWithBriefProvider : BaseItemProvider<HomeBaseItem>() {
     }
 
     override fun convert(helper: BaseViewHolder, item: HomeBaseItem) {
+
+        val binding =
+            DataBindingUtil.getBinding<ItemVideocollectionwithbriefBinding>(helper.itemView)
+        val videoCollectionWithBrief = item.data as CollectionItemCard
+        val itemList = videoCollectionWithBrief.itemList
+
+        val adapter = VideoCollectionWithBriefFooterAdapter()
+
+        binding?.run {
+            if (itemList != null && itemList.size > 0) {
+                recyVcwb.layoutManager =
+                    LinearLayoutManager(root.context, LinearLayoutManager.VERTICAL, false)
+                recyVcwb.setHasFixedSize(true)
+                recyVcwb.adapter = adapter
+                itemList.forEach {
+                    adapter.setAllDatas(it.data!!.tags)
+                    this.videoCollectionWithBriefData = it.data
+                    executePendingBindings()
+                }
+
+            }
+
+
+        }
 
     }
 
